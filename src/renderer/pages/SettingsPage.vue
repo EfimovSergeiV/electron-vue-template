a3bb949d34a1dbb03279e2268f24cabfcd07a424
Sergei Efimov <efimovsergeiv@gmail.com>
2025-09-09 21:23:13 +0300

    add: Pinia

--- src/renderer/pages/SettingsPage.vue
+++ src/renderer/pages/SettingsPage.vue
@@ -1,6 +1,19 @@
 <script setup>
   import { ref } from 'vue';
 
+
+  import { useCounterStore } from '../stores/counter'
+
+  const counter = useCounterStore()
+
+  counter.count++
+  // with autocompletion ✨
+  counter.$patch({ count: counter.count + 1 })
+  // or using an action instead
+  counter.increment()
+
+
+
   const isDark = ref(false);
 
   function toggleTheme() {
@@ -13,6 +26,8 @@
 <template>
   <div class="p-8 text-center">
     <h1 class="text-red-600 dark:text-blue-400 text-3xl font-bold mb-4">Settings</h1>
+
+    <div>Current Count: {{ counter.count }}</div>
 
     <button @click="toggleTheme">
       Переключить тему
