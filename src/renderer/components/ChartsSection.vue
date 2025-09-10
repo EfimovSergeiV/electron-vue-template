aaaf0e741788df0a655a419fe960c4adbc2c184a
EfimovSergeiV <efimovsergeiv@gmail.com>
2025-09-10 11:30:07 +0300

    Add:

--- src/renderer/components/ChartsSection.vue
+++ src/renderer/components/ChartsSection.vue
@@ -22,7 +22,7 @@
 
 
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-6">
+    <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-8 mt-6">
 
 
     <div class="">
