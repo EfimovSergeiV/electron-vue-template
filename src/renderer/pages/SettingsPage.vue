efa1f9b8c7f06591578a41ea8ab074cb81f2c74a
EfimovSergeiV <efimovsergeiv@gmail.com>
2025-12-12 16:41:46 +0300

    add:

--- src/renderer/pages/SettingsPage.vue
+++ src/renderer/pages/SettingsPage.vue
@@ -43,12 +43,16 @@
     });
   });
 
+
+  const buttonStatus = ref(false)
   const runPythonScript = async () => {
     window.electronAPI.runPython();
+    buttonStatus.value = true
   }
 
   const stopPython = () => {
     window.electronAPI.stopPython();
+    buttonStatus.value = false
   };
 
 </script>
@@ -58,8 +62,10 @@
   <div class="min-h-screen container mx-auto px-2 text-gray-800 dark:text-blue-500">
   
     <div class="flex gap-4 mb-4">
-      <button @click="runPythonScript()" class="bg-blue-500 px-6 py-2 rounded-sm text-white font-semibold">Запустить Python скрипт</button>
-      <button @click="stopPython()" class="bg-blue-500 px-6 py-2 rounded-sm text-white font-semibold">Остановить Python скрипт</button>
+      <transition name="page" mode="out-in">
+        <button v-if="buttonStatus" @click="stopPython()" class="bg-blue-500 px-6 py-2 rounded-sm text-white font-semibold w-[320px]">Остановить Python скрипт</button>
+        <button v-else @click="runPythonScript()" class="bg-blue-500 px-6 py-2 rounded-sm text-white font-semibold w-[320px]">Запустить Python скрипт</button>
+      </transition>
     </div>
     
     <div v-for="(value, i) in pyResult.slice(-5)" :key="i" class="">
@@ -67,13 +73,13 @@
     </div>
 
 
-    <!-- <div class="my-6 pb-1 border-b border-black/10 dark:border-white/10">
+    <div class="my-6 pb-1 border-b border-black/10 dark:border-white/10">
       <h1 class="text-3xl text-gray-800 dark:text-blue-500">{{ $t('menu.settings') }}</h1>
-    </div> -->
+    </div>
     
 
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-      <!-- <div class="flex flex-col gap-4">
+      <div class="flex flex-col gap-4">
         <div class="flex items-center justify-between gap-8 border-b border-black/10 dark:border-white/10">
           <div class="">
             <label for="locale-select">{{ $t('menu.language') }}</label>
@@ -98,11 +104,11 @@
           </div>
         </div>
         <div v-for="i in 10" class="flex items-center justify-between gap-8 border-b border-black/10 dark:border-white/10"><p>{{ $t('test.PropertyName') }} {{ i }}</p> <p>{{ $t('test.PropertyValue') }} {{ i }}</p></div>
-      </div> -->
+      </div>
 
 
 
-      <!-- <form class="language">
+      <form class="language">
         <label for="locale-select">{{ $t('menu.language') }}</label>
         <select id="locale-select" v-model="locale">
           <option
@@ -113,7 +119,7 @@
             {{ availableLocale }}
           </option>
         </select>
-      </form> -->
+      </form>
 
 
     </div>
